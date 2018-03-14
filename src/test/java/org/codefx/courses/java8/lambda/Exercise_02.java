602a3006f894218beb1e37558beb34deb76d3bf4
Nicolai Parlog <nipa@codefx.org>
2018-03-14 14:59:55 +0100

    Create lambda exercise 2

---
+++ src/test/java/org/codefx/courses/java8/lambda/Exercise_02.java
@@ -0,0 +1,68 @@
+package org.codefx.courses.java8.lambda;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class Exercise_02 {
+
+	/*
+	 * TASK: Replace the three anonymous classes assigned to
+	 *       `evenFilter`, `twoIncrementer`, `sysOutPrinter`
+	 *       with lambda expressions
+	 */
+
+	private final IntFilter evenFilter = new IntFilter() {
+		@Override
+		public boolean pass(int integer) {
+			return integer % 2 == 0;
+		}
+	};
+
+	private interface IntFilter {
+
+		boolean pass(int integer);
+
+	}
+
+	private final IntIncrementer twoIncrementer = new IntIncrementer() {
+		@Override
+		public int increment(int integer) {
+			return integer + 2;
+		}
+	};
+
+	private interface IntIncrementer {
+
+		int increment(int integer);
+
+	}
+
+	private final IntPrinter sysOutPrinter = new IntPrinter() {
+		@Override
+		public void print(int integer) {
+			System.out.println(integer);
+		}
+	};
+
+	private interface IntPrinter {
+
+		void print(int integer);
+
+	}
+
+	@Test
+	void evenFilter() {
+		assertThat(evenFilter.pass(0)).isTrue();
+		assertThat(evenFilter.pass(1)).isFalse();
+		assertThat(evenFilter.pass(2)).isTrue();
+	}
+
+	@Test
+	void twoIncrementer() {
+		assertThat(twoIncrementer.increment(0)).isEqualTo(2);
+		assertThat(twoIncrementer.increment(5)).isEqualTo(7);
+		assertThat(twoIncrementer.increment(10)).isEqualTo(12);
+	}
+
+}
