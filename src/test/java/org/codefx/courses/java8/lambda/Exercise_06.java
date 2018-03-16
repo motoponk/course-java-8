880855c6b31b043442a12b3fbe3469c13c9a6e07
Nicolai Parlog <nipa@codefx.org>
2018-03-16 15:01:14 +0100

    Create lambda exercise 6

---
+++ src/test/java/org/codefx/courses/java8/lambda/Exercise_06.java
@@ -0,0 +1,10 @@
+package org.codefx.courses.java8.lambda;
+
+public class Exercise_06 {
+
+	/*
+	 * TASK: Copy a handful of fields and methods from exercise 5 and use method references
+	 *       instead of lambda expressions.
+	 */
+
+}
