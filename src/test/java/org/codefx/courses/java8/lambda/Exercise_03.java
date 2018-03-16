46354463d14ef834c5698461935f5278e4aa1058
Nicolai Parlog <nipa@codefx.org>
2018-03-16 12:53:20 +0100

    Solve lambda exercise 3

--- src/test/java/org/codefx/courses/java8/lambda/Exercise_03.java
+++ src/test/java/org/codefx/courses/java8/lambda/Exercise_03.java
@@ -16,10 +16,10 @@ public class Exercise_03 {
 	 */
 
 	/** Computes the length (or rather, size) of the list. */
-	private ListCruncher<String> listLength = null;
+	private ListCruncher<String> listLength = list -> list.size();
 
 	/** Computes the sum of the lengths of the strings in the list. */
-	private ListCruncher<String> stringLengths = null;
+	private ListCruncher<String> stringLengths = list -> list.stream().mapToInt(String::length).sum();
 
 	private interface ListCruncher<T> {
 
@@ -28,10 +28,10 @@ public class Exercise_03 {
 	}
 
 	/** Adds all ints in the list or returns the default value if the list is empty. */
-	private ListAccumulator adder = null;
+	private ListAccumulator adder = (ints, def) -> ints.stream().reduce((i, j) -> i + j).orElse(def);
 
 	/** Multiplies all ints in the list or returns the default value if the list is empty. */
-	private ListAccumulator multiplier = null;
+	private ListAccumulator multiplier = (ints, def) -> ints.stream().reduce((i, j) -> i * j).orElse(def);
 
 	private interface ListAccumulator {
 
@@ -39,35 +39,35 @@ public class Exercise_03 {
 
 	}
 
-//	@Test
-//	void listLengthCruncher() {
-//		assertThat(listLength.crunch(asList())).isEqualTo(0);
-//		assertThat(listLength.crunch(asList(""))).isEqualTo(1);
-//		assertThat(listLength.crunch(asList("A", "BC"))).isEqualTo(2);
-//		assertThat(listLength.crunch(asList("A", "BC", "D, EF"))).isEqualTo(3);
-//	}
+	@Test
+	void listLengthCruncher() {
+		assertThat(listLength.crunch(asList())).isEqualTo(0);
+		assertThat(listLength.crunch(asList(""))).isEqualTo(1);
+		assertThat(listLength.crunch(asList("A", "BC"))).isEqualTo(2);
+		assertThat(listLength.crunch(asList("A", "BC", "D, EF"))).isEqualTo(3);
+	}
 
-//	@Test
-//	void stringLengthsCruncher() {
-//		assertThat(stringLengths.crunch(asList())).isEqualTo(0);
-//		assertThat(stringLengths.crunch(asList(""))).isEqualTo(0);
-//		assertThat(stringLengths.crunch(asList("A"))).isEqualTo(1);
-//		assertThat(stringLengths.crunch(asList("A", "BC"))).isEqualTo(3);
-//		assertThat(stringLengths.crunch(asList("A", "BC", "D, EF"))).isEqualTo(8);
-//	}
+	@Test
+	void stringLengthsCruncher() {
+		assertThat(stringLengths.crunch(asList())).isEqualTo(0);
+		assertThat(stringLengths.crunch(asList(""))).isEqualTo(0);
+		assertThat(stringLengths.crunch(asList("A"))).isEqualTo(1);
+		assertThat(stringLengths.crunch(asList("A", "BC"))).isEqualTo(3);
+		assertThat(stringLengths.crunch(asList("A", "BC", "D, EF"))).isEqualTo(8);
+	}
 
-//	@Test
-//	void adder(){
-//		assertThat(adder.accumulate(asList(), 5)).isEqualTo(5);
-//		assertThat(adder.accumulate(asList(0), 5)).isEqualTo(0);
-//		assertThat(adder.accumulate(asList(1, 5, 3), 5)).isEqualTo(9);
-//	}
+	@Test
+	void adder(){
+		assertThat(adder.accumulate(asList(), 5)).isEqualTo(5);
+		assertThat(adder.accumulate(asList(0), 5)).isEqualTo(0);
+		assertThat(adder.accumulate(asList(0), 5)).isEqualTo(0);
+	}
 
-//	@Test
-//	void multiplier(){
-//		assertThat(multiplier.accumulate(asList(), 5)).isEqualTo(5);
-//		assertThat(multiplier.accumulate(asList(0), 5)).isEqualTo(0);
-//		assertThat(multiplier.accumulate(asList(1, 5, 3), 5)).isEqualTo(15);
-//	}
+	@Test
+	void multiplier(){
+		assertThat(multiplier.accumulate(asList(), 5)).isEqualTo(5);
+		assertThat(multiplier.accumulate(asList(0), 5)).isEqualTo(0);
+		assertThat(multiplier.accumulate(asList(1, 5, 3), 5)).isEqualTo(15);
+	}
 
 }
