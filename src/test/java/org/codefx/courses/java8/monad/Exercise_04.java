c06aa04f5bf74c95aea803a78a9cfda1b9efb1ca
Nicolai Parlog <nipa@codefx.org>
2018-12-19 08:16:12 +0100

    Solve monad exercise 4

--- src/test/java/org/codefx/courses/java8/monad/Exercise_04.java
+++ src/test/java/org/codefx/courses/java8/monad/Exercise_04.java
@@ -2,7 +2,9 @@ package org.codefx.courses.java8.monad;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Optional;
 import java.util.concurrent.atomic.LongAdder;
+import java.util.function.Function;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -14,14 +16,31 @@ public class Exercise_04 {
 
 	@Test
 	void rightIdentity() {
+		// no way to break it
 	}
 
 	@Test
 	void leftIdentity() {
+		String v = null;
+		Function f = s -> "mango";
+
+		Optional applied = Optional.ofNullable(f.apply(v));
+		Optional mapped = Optional.ofNullable(v).map(f);
+
+		assertThat(applied).isNotEqualTo(mapped);
+
 	}
 
 	@Test
 	void associativity() {
+		String v = "kiwi";
+		Function f = s -> null;
+		Function g = s -> "mango";
+
+		Optional mapped = Optional.of(v).map(f).map(g);
+		Optional composed = Optional.of(v).map(f.andThen(g));
+
+		assertThat(composed).isNotEqualTo(mapped);
 	}
 
 }
